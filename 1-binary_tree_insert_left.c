da89eaedffd1f515d821a5e2029ae0b85be60159
JohanVillard <styx.locke@gmail.com>
2024-08-05 13:49:09 +0200

    Add NULL new node check

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,6 +15,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	new_node = binary_tree_node(parent, value);	/* Initialize a new node */
+	if (new_node == NULL)
+	return (NULL);
 
 	new_node->parent = parent;	/* Define parent of new_node */
 	if (parent->left != NULL)	/* Parent had left child */
