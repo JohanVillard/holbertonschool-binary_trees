c8c169a4e501831b44f965352da487a652c04fd8
JohanVillard <styx.locke@gmail.com>
2024-08-05 15:59:32 +0200

    Add all prototypes of mandatory tasks

--- binary_trees.h
+++ binary_trees.h
@@ -25,42 +25,43 @@ typedef struct binary_tree_s
 /* ----------------------FUNCTIONS------------------------ */
 
 void binary_tree_print(const binary_tree_t *);
+/* P0T0 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 /* P0T1 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 /* P0T2 */
-
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 /* P0T3 */
 void binary_tree_delete(binary_tree_t *tree);
 /* P0T4 */
-
+int binary_tree_is_leaf(const binary_tree_t *node);
 /* P0T5 */
 int binary_tree_is_root(const binary_tree_t *node);
 /* P0T6 */
-
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 /* P0T7 */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 /* P0T8 */
-
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 /* P0T9 */
 size_t binary_tree_height(const binary_tree_t *tree);
 /* P0T10 */
-
+size_t binary_tree_depth(const binary_tree_t *tree);
 /* P0T11 */
-
+size_t binary_tree_size(const binary_tree_t *tree);
 /* P0T12 */
-
+size_t binary_tree_leaves(const binary_tree_t *tree);
 /* P0T13 */
-
+size_t binary_tree_nodes(const binary_tree_t *tree);
 /* P0T14 */
-
+int binary_tree_balance(const binary_tree_t *tree);
 /* P0T15 */
-
+int binary_tree_is_full(const binary_tree_t *tree);
 /* P0T16 */
-
+int binary_tree_is_perfect(const binary_tree_t *tree);
 /* P0T17 */
-
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 /* P0T18 */
-
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 #endif /* _BINARY_TREES_H_ */
