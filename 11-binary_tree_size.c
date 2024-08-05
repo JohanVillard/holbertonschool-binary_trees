d37942bf5b0e3a31fb5bd05f2aaa4acd1cd33b47
JohanVillard <styx.locke@gmail.com>
2024-08-05 19:17:40 +0200

    Delete useless variable

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,12 +8,9 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	const binary_tree_t *current_node = tree;
-
-	if (current_node == NULL)	/* Base case */
+	if (tree == NULL)	/* Base case */
 		return (0);
 
 	/* Size(A) = 1 + Size(Aleft) + Size(Aright) */
 	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
-
 }
