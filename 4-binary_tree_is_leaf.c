b111f08d4bb05f1f68abc971fae628413117ddb6
NChaouch <nourchaouch44@gmail.com>
2024-08-06 12:25:03 +0200

    pipe in if task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,8 +9,10 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	while (node != NULL)
 	{
 		/* check if child left and right are not a leaf  */
-		if (node->left != NULL && node->right != NULL)
+		if (node->left != NULL || node->right != NULL)
+		{
 			return (0); /* if the node is not a leaf */
+		}
 		return (1); /* if the node is a leaf */
 	}
 	return (0);
